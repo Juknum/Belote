be77bd665459f6434343b49ccfacae3c582efd80
Julien <Juknum@users.noreply.github.com>
2020-04-10 16:50:04 +0200

    ajout du tableau contenant les 32 cartes

---
+++ projet/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void){
+	char *TabValCol[] = {"7 Pique", "8 Pique", "9 Pique", "10 Pique", "Valet Pique", "Dame Pique", "Roi Pique", "As Pique", "7 Carreau", "8 Carreau", "9 Carreau", "10 Carreau", "Valet Carreau", "Dame Carreau", "Roi Carreau", "As Carreau", "7 Coeur", "8 Coeur", "9 Coeur", "10 Coeur", "Valet Coeur", "Dame Coeur", "Roi Coeur", "As Coeur", "7 Trefle", "8 Trefle", "9 Trefle", "10 Trefle", "Valet Trefle", "Dame Trefle", "Roi Trefle", "As Trefle"};
+
+	// DEBUG : Affiche l'ensemble des cartes
+	/*
+	for(int i = 0; i < 32; i++){
+			printf("%s\n",TabValCol[i]);
+		}
+	*/
+
+return 0;
+}
